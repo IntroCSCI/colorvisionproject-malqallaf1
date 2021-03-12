79e8bc89e061d4292e3515dd7b8c3f6d5a0e07c7
Mohammad Alqallaf <malqallaf1@mail.csuchico.edu>
2021-03-12 14:01:46 +0000

    the code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-//Author: PUT YOUR NAME HERE
+//Author: Mohammed Al-Qallaf
 #include <iostream>
 using namespace std;
+<!Doctype html>
 
 int main()
 {
-
+ <title> CSS FILE COLOR BLINDNESS "True" <title>
+ <style>
+ body {
+   colors: #2C4617, #FFD008, #05A5F7, #FF018B
+ }
+ <style>
   return 0;
 }
